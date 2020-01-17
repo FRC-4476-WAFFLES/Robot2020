2df9a84c25a07b35f78721341df253e8d6b841a2
Ben Alderson <ben.chandler.alderson@gmail.com>
2020-01-17 18:58:37 -0500

    Add back colour sensor

--- Robot2020/src/main/java/frc/robot/subsystems/ColourWheelThingySubsystem.java
+++ Robot2020/src/main/java/frc/robot/subsystems/ColourWheelThingySubsystem.java
@@ -9,14 +9,15 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-/// import com.revrobotics.ColorSensorV3; 
+import com.revrobotics.ColorSensorV3;
 import frc.robot.Constants;
-// import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ColourWheelThingySubsystem extends SubsystemBase {
   TalonSRX colourWheelMotor = new TalonSRX(Constants.COLOUR_WHEEL_MANIPULATOR);
-  // private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  /// private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
   /**
    * Creates a new ColourWheelThingySubsystem.
@@ -27,6 +28,18 @@ public class ColourWheelThingySubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("ColorWheelThingySubsystem/hue", getHue());
   }
+
+  private double getHue() {
+    double R = m_colorSensor.getRed();
+    double G = m_colorSensor.getGreen();
+    double B = m_colorSensor.getBlue();
+    
+    double hue = (Math.atan2(Math.sqrt(3) * (G - B), 2*R - G - B) / Math.PI) * 180;
+
+    return hue < 0 ? 360 + hue : hue;
+  }
+  
+  
 }
