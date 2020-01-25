c597abd7d2782697168d0dcc9a132554bc8d71f3
Ben Alderson <ben.chandler.alderson@gmail.com>
2020-01-25 17:11:40 -0500

    Fix CommandSwitch

--- Robot2020/src/main/java/frc/robot/commands/Utility/CommandSwitch.java
+++ Robot2020/src/main/java/frc/robot/commands/Utility/CommandSwitch.java
@@ -7,38 +7,43 @@
 
 package frc.robot.commands.Utility;
 
-import java.util.function.Supplier;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SelectCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
  * Add your docs here.
  */
-public class CommandSwitch extends SelectCommand {
-    private static class SwitchSupplier implements Supplier<Command> {
-        private int i = -1;
-        private Command[] list;
-
-        public SwitchSupplier(Command... commands) {
-            list = commands;
-        }
-
-        @Override
-        public Command get() {
-            i++;
-
-            if(i >= list.length) {
-                i = 0;
-            }
-
-            System.out.println(i);
-
-            return list[i];
-        }
-    }
+public class CommandSwitch extends CommandBase {
+    private int i = -1;
+    private final Command[] list;
 
     public CommandSwitch(Command... commands) {
-        super(new SwitchSupplier(commands));
+        list = commands;
+    }
+
+    @Override
+    public void initialize() {
+        i++;
+        if(i > list.length) {
+            i = 0;
+        }
+
+        list[i].initialize();
+    }
+
+    @Override
+    public void execute() {
+        list[i].execute();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        list[i].end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return list[i].isFinished();
     }
 }
 
