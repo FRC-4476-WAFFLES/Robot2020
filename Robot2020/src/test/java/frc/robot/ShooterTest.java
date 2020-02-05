95afe4284cb6f562611785c05e45ab62fd338f87
Ben Alderson <ben.chandler.alderson@gmail.com>
2020-02-05 17:10:46 -0500

    Add shooter spin up test Also fix a few bugs in the simulation

---
+++ Robot2020/src/test/java/frc/robot/ShooterTest.java
@@ -0,0 +1,39 @@
+package frc.robot;
+
+import org.junit.jupiter.api.Test;
+
+import com.snobot.simulator.joysticks.joystick_specializations.XboxButtonMap;
+
+import org.junit.jupiter.api.Assertions;
+
+public class ShooterTest extends BaseTestFixture {
+    @Test
+    public void testSpinUpSpinDown() throws Exception {
+        // Check that the wheel starts stopped
+        mSimulator.getJoystick(2).setButton(XboxButtonMap.X_BUTTON, false);
+        simulateForTime(1.0);
+        Assertions.assertTrue(
+            approxZero(mSimulator.getTalonVelocity(Constants.SHOOTER_MASTER)),
+            "Shooter motor should start stopped");
+
+        for(int i=0; i<3; i++) {
+            // Check that wheel spins up after toggling
+            mSimulator.getJoystick(2).setButton(XboxButtonMap.X_BUTTON, true);
+            simulateForTime(0.1);
+            mSimulator.getJoystick(2).setButton(XboxButtonMap.X_BUTTON, false);
+            simulateForTime(0.2);
+            Assertions.assertTrue(
+                !approxZero(mSimulator.getTalonVelocity(Constants.SHOOTER_MASTER)),
+                "Shooter motor should spin up");
+
+            // Check that the wheel stops again
+            mSimulator.getJoystick(2).setButton(XboxButtonMap.X_BUTTON, true);
+            simulateForTime(0.1);
+            mSimulator.getJoystick(2).setButton(XboxButtonMap.X_BUTTON, false);
+            simulateForTime(0.2);
+            Assertions.assertTrue(
+                approxZero(mSimulator.getTalonVelocity(Constants.SHOOTER_MASTER)),
+                "Shooter motor should spin down");
+        }
+    }
+}
